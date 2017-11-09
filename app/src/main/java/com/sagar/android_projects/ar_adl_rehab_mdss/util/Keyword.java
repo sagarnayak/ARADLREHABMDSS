1be44d892dd299064ec7fc3b41024720424221f5
sagar <snkumar.nayak@gmail.com>
2017-11-09 18:02:45 +0530

    commit

---
+++ app/src/main/java/com/sagar/android_projects/ar_adl_rehab_mdss/util/Keyword.java
@@ -0,0 +1,9 @@
+package com.sagar.android_projects.ar_adl_rehab_mdss.util;
+
+/**
+ * Created by sagar on 11/9/2017.
+ */
+public class Keyword {
+    public static final String SHARED_PREFERENCE_NAME = "AR-ADL-REHAB-MDSS_DB";
+    public static final String IS_LOGGED_IN = "logged_in";
+}
