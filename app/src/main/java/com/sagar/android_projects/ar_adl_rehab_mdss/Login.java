89b631374b89dab938d6140ea35b2de94aacaf6f
sagar <snkumar.nayak@gmail.com>
2017-11-09 14:16:29 +0530

    commit

--- app/src/main/java/com/sagar/android_projects/ar_adl_rehab_mdss/Login.java
+++ app/src/main/java/com/sagar/android_projects/ar_adl_rehab_mdss/Login.java
@@ -1,13 +1,36 @@
 package com.sagar.android_projects.ar_adl_rehab_mdss;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
+
+    private EditText editTextUserName;
+    private EditText editTextPassword;
+    private Button buttonLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        editTextUserName = findViewById(R.id.edittext_username_login);
+        editTextPassword = findViewById(R.id.edittext_password_login);
+        buttonLogin = findViewById(R.id.button_login);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gotoDashBoard();
+            }
+        });
+    }
+
+    private void gotoDashBoard() {
+        startActivity(new Intent(Login.this, Dashboard.class));
     }
 }
