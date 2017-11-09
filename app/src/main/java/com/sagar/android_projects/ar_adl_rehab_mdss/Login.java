e872696eaceb7fa1231e9faaca396e1003f256e9
sagar <snkumar.nayak@gmail.com>
2017-11-09 13:49:53 +0530

    Initial commit

---
+++ app/src/main/java/com/sagar/android_projects/ar_adl_rehab_mdss/Login.java
@@ -0,0 +1,13 @@
+package com.sagar.android_projects.ar_adl_rehab_mdss;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
