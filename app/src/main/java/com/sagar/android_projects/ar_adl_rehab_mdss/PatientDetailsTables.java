505f851f1463373042fb961fc999ed51fd3fe28f
sagar <snkumar.nayak@gmail.com>
2017-11-16 18:06:13 +0530

    commit

--- app/src/main/java/com/sagar/android_projects/ar_adl_rehab_mdss/PatientDetailsTables.java
+++ app/src/main/java/com/sagar/android_projects/ar_adl_rehab_mdss/PatientDetailsTables.java
@@ -1,18 +1,23 @@
 package com.sagar.android_projects.ar_adl_rehab_mdss;
 
 import android.os.Bundle;
-import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.sagar.android_projects.ar_adl_rehab_mdss.adapter.AdapterPatientDetailsList;
 
 public class PatientDetailsTables extends AppCompatActivity {
 
     private RecyclerView recyclerView;
-    CollapsingToolbarLayout collapsingToolbar;
+    private TextView textViewName;
+    private TextView textviewAgeAndGender;
+    private TextView textViewCondition;
+    private TextView textViewMobileAndEmail;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +27,11 @@ public class PatientDetailsTables extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         recyclerView = findViewById(R.id.recyclerview_patient_detail);
+        textViewName = findViewById(R.id.textview_name_patient_details);
+        textviewAgeAndGender = findViewById(R.id.textview_age_and_gender_patient_details);
+        textViewCondition = findViewById(R.id.textview_condition_patient_details);
+        textViewMobileAndEmail = findViewById(R.id.textview_mobile_and_email_patient_details);
+        progressBar = findViewById(R.id.progressbar_patient_details);
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
