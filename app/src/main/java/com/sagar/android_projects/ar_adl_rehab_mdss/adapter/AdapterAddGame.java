f3db0f0f119cc48b689f158043ace4c63cc4cd3b
SAGAR <snkumar.nayak@gmail.com>
2017-11-30 03:53:32 +0530

    commit.

--- app/src/main/java/com/sagar/android_projects/ar_adl_rehab_mdss/adapter/AdapterAddGame.java
+++ app/src/main/java/com/sagar/android_projects/ar_adl_rehab_mdss/adapter/AdapterAddGame.java
@@ -12,9 +12,6 @@ import com.sagar.android_projects.ar_adl_rehab_mdss.retrofit.Models.game.Game;
 
 import java.util.ArrayList;
 
-/**
- * Created by SAGAR on 11/29/2017.
- */
 
 public class AdapterAddGame extends RecyclerView.Adapter<AdapterAddGame.ViewHolder> {
 
